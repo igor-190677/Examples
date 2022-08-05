f58bf94fb9df262f10f4cdb8907e8fcf2995628e
@igor_mashkin <igor-190677@yandex.ru>
2022-08-05 18:15:37 +0300

    Add Example_06 max

---
+++ Example_06 max/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 9;
+int b = 2;
+int c = 11;
+int d = 16;
+int e = 8;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Максимальный вес гири равен: ");
+Console.Write(max);
+Console.WriteLine(" кг.");
