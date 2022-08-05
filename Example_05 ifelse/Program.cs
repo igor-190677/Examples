d3860e14376b0a82fb6a5d0147c331b7bf84b24a
@igor_mashkin <igor-190677@yandex.ru>
2022-08-05 18:05:32 +0300

    Add Example_05 if else

---
+++ Example_05 ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿var user_name = (string?)null;
+Console.Write("Ввведите имя пользователя: ");
+user_name = Console.ReadLine();
+if(user_name.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(user_name);
+    Console.WriteLine("!");
+}
