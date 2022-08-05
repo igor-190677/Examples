b1e1ff6962a8987c2dec702ea5b7b739f65d566e
@igor_mashkin <igor-190677@yandex.ru>
2022-08-05 16:13:57 +0300

    Add change Example_02/Program.cs

---
+++ Example_02/Program.cs
@@ -0,0 +1,5 @@
+﻿var user_name = (string?)null;
+Console.Write("Введите ваше имя:");
+user_name = Console.ReadLine();
+Console.Write("Hello,");
+Console.Write(user_name);
---
+++ Exapmle_03_prog/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberA = new Random().Next(1, 10);
+Console.Write("Первое число: ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write("Второе число: ");
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write("Результат: ");
+Console.WriteLine(result);
+
+//Сделано.
