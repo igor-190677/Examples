1a98a4c8d7341e2e936b7b5ae575fcfd4f497491
@igor_mashkin <igor-190677@yandex.ru>
2022-08-05 16:19:45 +0300

    Add Example_04

---
+++ Example_04/Program.cs
@@ -0,0 +1,10 @@
+﻿double numberA = 12;
+Console.Write("Первое число: ");
+Console.WriteLine(numberA);
+double numberB = 5;
+Console.Write("Второе число: ");
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.Write("Результат: ");
+Console.WriteLine(result);
+
