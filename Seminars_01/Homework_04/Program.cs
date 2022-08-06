27f820e2a3b605a7c693acfe0a7495855356ee4c
@igor_mashkin <igor-190677@yandex.ru>
2022-08-06 11:48:04 +0300

    Add Homework_04 from Seminar_01

---
+++ Seminars_01/Homework_04/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число: ");
+int i_number = int.Parse(Console.ReadLine());
+
+if(i_number <= 1)
+{
+    Console.Write("Неверный диапазон чисел");
+}
+else
+{
+    Console.Write("Четные числа в диапазоне чисел от 1 до " + i_number + ": ");
+    for (int number = 2; number < i_number + 1; number += 2)
+    {
+            if (number == 2)
+        {
+            Console.Write(number);
+        }
+        else
+        {
+            Console.Write(", " + number);
+        }
+    }
+}
