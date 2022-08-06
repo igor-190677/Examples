55f3489a95b93e3c70c9bab5430b23ec3ef9d283
@igor_mashkin <igor-190677@yandex.ru>
2022-08-06 11:02:54 +0300

    Add Homework_02 from Seminar_01

---
+++ Seminars_01/Homework_02/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число: ");
+string first_string = Console.ReadLine();
+int number_1 = Convert.ToInt32(first_string);
+
+Console.Write("Введите второе число: ");
+string second_string = Console.ReadLine();
+int number_2 = Convert.ToInt32(second_string);
+
+Console.Write("Введите третье число: ");
+string third_string = Console.ReadLine();
+int number_3 = Convert.ToInt32(third_string);
+
+int max_number = number_1;
+if (number_2 > max_number)
+{
+    max_number = number_2;
+}
+if (number_3 > max_number)
+{
+    max_number = number_3;
+}
+
+Console.WriteLine("Максимальное число равно: " + max_number);
