0a466b99411edf531ca92f9218c169b60effc548
@igor_mashkin <igor-190677@yandex.ru>
2022-08-08 19:13:49 +0300

    Add Example_02 to Lesson_02

---
+++ Lesson_02/Example_02/Program.cs
@@ -0,0 +1,20 @@
+﻿int Max(int[] numbers)
+{
+    int maxNumber = numbers[0];
+    for (int i = 1; i < numbers.Length; i++)
+    {
+        if (numbers[i] > maxNumber) maxNumber = numbers[i];
+    }
+    return maxNumber;
+}
+
+int[] numbers = {12, 130, 14, 5, 16, 177, 18, 1999, 20};
+
+int maxNumber = Max(numbers);
+
+Console.WriteLine("Макисмальное число равно: " + maxNumber);
+
+
+
+
+
