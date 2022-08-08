134b61250aa37b92b93783de04f2357a7e2ce199
@igor_mashkin <igor-190677@yandex.ru>
2022-08-08 17:35:06 +0300

    Remove files to Lesson_01

--- Lesson_01/Example_07_triangle/Program.cs
+++ Lesson_01/Example_07_triangle/Program.cs
@@ -4,7 +4,7 @@ int xa = 40, ya = 1,
 xb = 1, yb = 30, 
 xc = 80, yc = 30;
 
-Console.SetCursorPosition(xa, xb);
+Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 
 Console.SetCursorPosition(xb, yb);
@@ -13,7 +13,7 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-int x = xa, y = ya;
+int x = xa, y = xb;
 
 int count = 0;
 
@@ -23,21 +23,23 @@ while (count < 10000)
     int what = new Random().Next(0, 3);
     if (what == 0);
     {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
+        x = (x + xa)/2;
+        y = (y + ya)/2;
     }
+
     if (what == 1);
     {
-        x = (x + xb) / 2;
-        y = (y + yb) / 2;    
+        x = (x + xb)/2;
+        y = (y + yb)/2;    
     }
+
     if (what == 2);
     {
-        x = (x + xc) / 2;
-        y = (y + yc) / 2;    
+        x = (x + xc)/2;
+        y = (y + yc)/2;    
     }
 
     Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
+    Console.WriteLine(what);
     count++;
 }
