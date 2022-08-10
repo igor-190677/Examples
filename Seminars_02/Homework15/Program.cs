eeed0ef2311b42fc6f13956c4bf1d918063abeae
@igor_mashkin <igor-190677@yandex.ru>
2022-08-10 18:34:54 +0300

    Add change in homework15 for Seminars02

--- Seminars_02/Homework15/Program.cs
+++ Seminars_02/Homework15/Program.cs
@@ -1,11 +1,16 @@
-﻿int[] daysWeekend = new int[] {6, 7};
-int weekend  = 0;
+﻿void FindWeekend(int[] Array, int day)
+{
+    int weekend  = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (day == Array[i]) {weekend = day; Console.WriteLine("Да"); break;};
+    }
+    if (weekend == 0) {Console.WriteLine("Нет");}
+}
+
+int[] daysWeekend = new int[] {6, 7};
 
 Console.Write("Введите день недели: ");
 int numberDay = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < daysWeekend.Length; i++)
-{
-    if (numberDay == daysWeekend[i]) {weekend = numberDay; Console.WriteLine("Да"); break;};
-}
-if (weekend == 0) {Console.WriteLine("Нет");}
+FindWeekend(daysWeekend, numberDay);
