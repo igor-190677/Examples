1382508ce2c4238b4a293b29ae55c2a65983d24b
@igor_mashkin <igor-190677@yandex.ru>
2022-08-10 18:42:39 +0300

    Add change in homework13 for Seminars02

--- Seminars_02/Homework13/Program.cs
+++ Seminars_02/Homework13/Program.cs
@@ -1,16 +1,21 @@
-﻿Console.Write("Ведите число: ");
+﻿void FindNumber(string number, int index)
+{
+    char[] Array = number.ToCharArray();
+
+    if (Array.Length < 3)
+    {
+        Console.WriteLine("Третьей цифры нет.");
+    }
+    else
+    {
+        Console.Write("Третья цифра в числе: ");
+        Console.WriteLine(Array[index]);
+    }
+}
+
+Console.Write("Ведите число: ");
 string number = Console.ReadLine();
 
-char[] Array = number.ToCharArray();
-
-if (Array.Length < 3)
-{
-    Console.WriteLine("Третьей цифры нет.");
-}
-else
-{
-    Console.Write("Третья цифра в числе: ");
-    Console.WriteLine(Array[2]);
-}
+FindNumber(number, 2);
 
 
