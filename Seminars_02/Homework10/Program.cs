d84d6ab8c6fe156c988bf3556c6062f2a05f4baa
@igor_mashkin <igor-190677@yandex.ru>
2022-08-10 17:44:06 +0300

    Add homework10 for Seminars02

---
+++ Seminars_02/Homework10/Program.cs
@@ -0,0 +1,9 @@
+﻿int index = 1;
+
+Console.Write("Ведите трехзначное число: ");
+string number = Console.ReadLine();
+
+char chr = number[index];
+
+Console.Write("Вторая цифра в числе: ");
+Console.WriteLine(chr);
---
+++ Seminars_02/Homework_online/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Ведите первое число: ");
+int namberA = int.Parse(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int namberB = int.Parse(Console.ReadLine());
+int remainder = namberA % namberB;
+if (remainder == 0)
+{
+    Console.Write("Числа являются кратными. Общий делитель равен: ");
+    Console.WriteLine(namberA / namberB);
+}
+else
+{
+    Console.Write("Числа не являются кратными. Остаток равен: ");
+    Console.WriteLine(remainder);
+}
