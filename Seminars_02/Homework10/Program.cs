ca18cca819921afb70ce2e4c00a5589ba855a88b
@igor_mashkin <igor-190677@yandex.ru>
2022-08-10 17:57:10 +0300

    Add change in homework10 for Seminars02

--- Seminars_02/Homework10/Program.cs
+++ Seminars_02/Homework10/Program.cs
@@ -1,9 +1,7 @@
-﻿int index = 1;
-
-Console.Write("Ведите трехзначное число: ");
+﻿Console.Write("Ведите трехзначное число: ");
 string number = Console.ReadLine();
 
-char chr = number[index];
+char chr = number[1];
 
 Console.Write("Вторая цифра в числе: ");
 Console.WriteLine(chr);
