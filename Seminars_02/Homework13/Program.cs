506e4ce725eff7f69f273dbce6bd3eecdb6f0cf2
@igor_mashkin <igor-190677@yandex.ru>
2022-08-10 17:55:22 +0300

    Add homework13 for Seminars02

---
+++ Seminars_02/Homework13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Ведите число: ");
+string number = Console.ReadLine();
+
+char[] Array = number.ToCharArray();
+
+if (Array.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    Console.Write("Третья цифра в числе: ");
+    Console.WriteLine(Array[2]);
+}
+
+
