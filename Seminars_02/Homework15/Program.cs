558a3e8b4873c2b8ac7b1015edbd9fe637e76aab
@igor_mashkin <igor-190677@yandex.ru>
2022-08-10 18:23:02 +0300

    Add homework15 for Seminars02

---
+++ Seminars_02/Homework15/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] daysWeekend = new int[] {6, 7};
+int weekend  = 0;
+
+Console.Write("Введите день недели: ");
+int numberDay = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < daysWeekend.Length; i++)
+{
+    if (numberDay == daysWeekend[i]) {weekend = numberDay; Console.WriteLine("Да"); break;};
+}
+if (weekend == 0) {Console.WriteLine("Нет");}
