43d8d25511a0bb197c26df76fa43d68e634c9aee
@igor_mashkin <igor-190677@yandex.ru>
2022-08-18 22:52:41 +0300

    Add Homework_23 for Seminars_03

---
+++ Seminars_03/Homework_23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+string numberString = Console.ReadLine();
+int numberUser = Convert.ToInt32(numberString);
+
+void PrintArrey(double[] Array)
+{
+    Console.Write(Array[0]);
+    for (int i = 1; i < Array.Length; i++) Console.Write(", " + Array[i]);
+}
+
+double[] FindSquads(int number)
+{
+    double[] numberSquads = new double[number];
+    for (int i = 0; i < (number); i++) numberSquads[i] = Math.Round(Math.Pow(i + 1, 3));
+    return numberSquads;
+}
+
+PrintArrey(FindSquads(numberUser));
