feb05b28e4da8a451b23d44ec4955c4c72061324
@igor_mashkin <igor-190677@yandex.ru>
2022-08-18 15:19:32 +0300

    Add Example_02 for Lesson_03

---
+++ Lesson_03/Example_02/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1, 8, 9, 10, 15, 14, 13, 20, 19, 18, 17, 16, 12, 11 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+Console.WriteLine();
+PrintArray(arr);
