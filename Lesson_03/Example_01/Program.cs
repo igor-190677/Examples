f7324a7ef03766c193123a655c1ba73602ccc846
@igor_mashkin <igor-190677@yandex.ru>
2022-08-18 15:20:10 +0300

    Add Example_01 for Lesson_03

---
+++ Lesson_03/Example_01/Program.cs
@@ -0,0 +1,20 @@
+﻿string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так крсноречивы. Вы дадите мне чаю?";
+// string s = "qwerty"
+//             012
+// s[3]// r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
