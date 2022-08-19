18496813453faf75b4f595eb8fb3fcb6413e0200
@igor_mashkin <igor-190677@yandex.ru>
2022-08-19 13:09:11 +0300

    Add in Homework_27 for Seminars_04

---
+++ Seminars_04/Homework_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SummaNumbers(string numberUser)
+{
+    char charOfNumber = 'a';
+    int sumNumbers = 0;
+    for (int i = 0; i < numberUser.Length; i++)
+    {
+        charOfNumber = numberUser[i];
+        int number = charOfNumber - '0';
+        sumNumbers += number;
+    }
+    return sumNumbers;
+}
+
+Console.Write("Введите число A: ");
+string numberString = Console.ReadLine();
+
+int result = SummaNumbers(numberString);
+Console.WriteLine("Сумма цифр в числе равна: " + result);
