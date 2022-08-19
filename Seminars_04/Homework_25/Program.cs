8b4f7f32fe814a53f6a826cbb0ae9f2db0a86bec
@igor_mashkin <igor-190677@yandex.ru>
2022-08-19 12:29:54 +0300

    Add Homework_25 for Seminars_04

---
+++ Seminars_04/Homework_25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+string numberStringA = Console.ReadLine();
+int numberUserA = Convert.ToInt32(numberStringA);
+
+Console.Write("Введите число B: ");
+string numberStringB = Console.ReadLine();
+int numberUserB = Convert.ToInt32(numberStringB);
+
+void PrintArrey(double result)
+{
+    Console.Write("A ^ B = ");
+    Console.Write(result);
+}
+
+double FindDegree(int numberA, int numberB)
+{
+    double degree = Math.Round(Math.Pow(numberA, numberB));
+    return degree;
+}
+
+PrintArrey(FindDegree(numberUserA, numberUserB));
