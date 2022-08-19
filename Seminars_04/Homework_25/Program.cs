44c8d443e81f7c291242ec836e82d5ce39a1abd2
@igor_mashkin <igor-190677@yandex.ru>
2022-08-19 12:32:28 +0300

    Add change in Homework_25 for Seminars_04

--- Seminars_04/Homework_25/Program.cs
+++ Seminars_04/Homework_25/Program.cs
@@ -2,14 +2,6 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Введите число A: ");
-string numberStringA = Console.ReadLine();
-int numberUserA = Convert.ToInt32(numberStringA);
-
-Console.Write("Введите число B: ");
-string numberStringB = Console.ReadLine();
-int numberUserB = Convert.ToInt32(numberStringB);
-
 void PrintArrey(double result)
 {
     Console.Write("A ^ B = ");
@@ -21,5 +13,13 @@ double FindDegree(int numberA, int numberB)
     double degree = Math.Round(Math.Pow(numberA, numberB));
     return degree;
 }
+
+Console.Write("Введите число A: ");
+string numberStringA = Console.ReadLine();
+int numberUserA = Convert.ToInt32(numberStringA);
+
+Console.Write("Введите число B: ");
+string numberStringB = Console.ReadLine();
+int numberUserB = Convert.ToInt32(numberStringB);
 
 PrintArrey(FindDegree(numberUserA, numberUserB));
