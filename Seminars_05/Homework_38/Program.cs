546af2e3239382b6abbbf09d8aa10bb4df77f659
@igor_mashkin <igor-190677@yandex.ru>
2022-08-22 19:39:37 +0300

    Add Homework_38 for Seminars_05

---
+++ Seminars_05/Homework_38/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int cells = 8;
+int[] ArreyUser = new int[cells];
+
+void PrintArrey(int[] Array)
+{
+    Console.Write("[" + Array[0]);
+    for (int i = 1; i < Array.Length; i++) Console.Write(", " + Array[i]);
+    Console.Write("]");
+}
+
+int[] GenerationArray(int numberCells)
+{
+    int[] UserArrey = new int[numberCells];
+    for (int i = 0; i < numberCells; i++)
+    {
+        int numberA = new Random().Next(0, 100);
+        UserArrey[i] = numberA;
+    }
+    return UserArrey;
+}
+
+int DifMaxMin(int[] numberCells)
+{
+    int min = numberCells[0];
+    int max = numberCells[0];
+    for (int i = 1; i < numberCells.Length; i++)
+    {
+        if (max < numberCells[i]) max = numberCells[i];
+        if (min > numberCells[i]) min = numberCells[i];
+    }
+    int difMinMax = max - min;
+    return difMinMax;
+}
+
+ArreyUser = GenerationArray(cells);
+PrintArrey(ArreyUser);
+
+int difNumberMaxMin = DifMaxMin(ArreyUser);
+Console.WriteLine();
+Console.WriteLine("Разница между максимальным и минимальным числами массива: " + difNumberMaxMin);
