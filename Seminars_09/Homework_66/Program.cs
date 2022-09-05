ff0fa8775229f016883da16047aa831584cc857a
@igor_mashkin <igor-190677@yandex.ru>
2022-09-05 20:47:03 +0300

    Add Homework_66 for Seminars_09

---
+++ Seminars_09/Homework_66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SummaNumbers(int numberM, int numberN)
+{
+    if (numberM == numberN) return numberN;
+    return numberM + SummaNumbers(numberM + 1, numberN); 
+}
+
+Console.Write("Введите натуральное число M: ");
+int numbM = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int numbN = Convert.ToInt16(Console.ReadLine());
+
+if (numbM < numbN) Console.WriteLine(SummaNumbers(numbM, numbN));
+if (numbM > numbN) Console.WriteLine(SummaNumbers(numbN, numbM));
