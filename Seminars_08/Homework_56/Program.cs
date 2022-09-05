0f12d0f0fd8df103ce9b527b8c946cb7507a33e6
@igor_mashkin <igor-190677@yandex.ru>
2022-09-05 19:33:40 +0300

    Add change Homework_56 for Seminars_08

--- Seminars_08/Homework_56/Program.cs
+++ Seminars_08/Homework_56/Program.cs
@@ -6,34 +6,34 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-void PrintArray(int[,] arreyUser)
+void PrintArray(int[,] arrayUser)
 {
-    for (int i = 0; i < arreyUser.GetLength(0); i++)
+    for (int i = 0; i < arrayUser.GetLength(0); i++)
     {
-        for (int j = 0; j < arreyUser.GetLength(1); j++)
+        for (int j = 0; j < arrayUser.GetLength(1); j++)
         {
-            Console.Write($"{arreyUser[i,j]}  ");
+            Console.Write($"{arrayUser[i,j]}  ");
         }
     Console.WriteLine();
     }
 }
 
-void FindStingWithMinSumNumbers(int[,] arreyUser)
+void FindStingWithMinSumNumbers(int[,] arrayUser)
 {
-    int[] singleArray = new int[arreyUser.GetLength(1)];
+    int[] singleArray = new int[arrayUser.GetLength(1)];
     int minSumNumbersString = 0;
     int sumNumbersString = 0;
 
-    for (int j = 0; j < arreyUser.GetLength(1); j++) minSumNumbersString += arreyUser[0, j];
-    for (int j = 0; j < arreyUser.GetLength(1); j++) singleArray[j] = arreyUser[0, j];
+    for (int j = 0; j < arrayUser.GetLength(1); j++) minSumNumbersString += arrayUser[0, j];
+    for (int j = 0; j < arrayUser.GetLength(1); j++) singleArray[j] = arrayUser[0, j];
 
-    for (int i = 1; i < arreyUser.GetLength(0); i++)
+    for (int i = 1; i < arrayUser.GetLength(0); i++)
     {
         sumNumbersString = 0;
-        for (int j = 0; j < arreyUser.GetLength(1); j++) sumNumbersString += arreyUser[i, j];
+        for (int j = 0; j < arrayUser.GetLength(1); j++) sumNumbersString += arrayUser[i, j];
         if (sumNumbersString < minSumNumbersString)
         {
-            for (int k = 0; k < arreyUser.GetLength(1); k++) singleArray[k] = arreyUser[i, k];
+            for (int k = 0; k < arrayUser.GetLength(1); k++) singleArray[k] = arrayUser[i, k];
         }
     }
     for (int i = 0; i < singleArray.GetLength(0); i++) Console.Write($"{singleArray[i]}  ");
