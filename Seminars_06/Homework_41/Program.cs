9346c4280556c3e4617905099150026fc89dc977
@igor_mashkin <igor-190677@yandex.ru>
2022-09-10 19:00:55 +0300

    Add Homework_41 for Seminars_06

---
+++ Seminars_06/Homework_41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void CounterNumbers(int numb)
+{
+    int numberUser = 0;
+    int counter = 0;
+    for (int i = 0; i < numb; i++)
+    {
+        Console.Write("Введите отрицательное или положительное целое число: ");
+        numberUser = Convert.ToInt16(Console.ReadLine());
+        if (numberUser > 0) counter++;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Чисел пользователя больше ноля: {counter}");
+}
+
+Console.Write("Сколько чисел вы будете вводить: ");
+int number = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine();
+
+CounterNumbers(number);
